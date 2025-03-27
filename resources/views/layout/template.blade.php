2884b294adbbdf7edf8c2457bcc689a6fdae2537
Sidkyelman <sidkyelman6@gmail.com>
2025-03-27 21:34:41 +0700

    add modified file

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -33,6 +33,7 @@
 
 <body>
     <div class="container-xxl position-relative bg-white d-flex p-0">
+        <h1>Halo apakabar</h1>
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
@@ -51,7 +52,7 @@
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
                         <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                        <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
+                        <div class="bg-success rounded-circle border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
                         <h6 class="mb-0">Jhon Doe</h6>
